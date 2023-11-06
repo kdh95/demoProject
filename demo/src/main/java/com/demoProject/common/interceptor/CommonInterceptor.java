e631a6a84e2523cb488e7ba95a8c68a1f14b1dcb
kdh95 <ehgks3323@gmail.com>
2023-11-06 20:09:49 +0900

    add Interceptor

---
+++ demo/src/main/java/com/demoProject/common/interceptor/CommonInterceptor.java
@@ -0,0 +1,20 @@
+package com.demoProject.common.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CommonInterceptor implements HandlerInterceptor {
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+    throws Exception {
+
+        return true;
+    }
+    
+}
